e186211458254b495ff00b2608ee7db3c4d02dff
Vaios Karampinis <oden@oden.gr>
2017-08-16 23:05:06 +0300

    make linter happy :)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ const SeasonMenu = () => (
   <div>
     <Divider/>
     <Header as='h5' color='grey' textAlign='center'>
-      This page was created with ❤ and some frozen 🍺.<br/>
-      Fill free to <a href="https://github.com/vkarampinis/football-stats" target="_blank">fork</a>, hack and learn! <br/>
-      All data provided by open API from <a href="http://football-data.org" target="_blank">football-data.org</a>
+      This page was created with <span role="img">❤</span> and some frozen <span role="img">🍺</span>.<br/>
+      Fill free to <a href="https://github.com/vkarampinis/football-stats" target="_blank" rel="noopener noreferrer">fork</a>, hack and learn! <br/>
+      All data provided by open API from <a href="http://football-data.org" target="_blank" rel="noopener noreferrer">football-data.org</a>
     </Header>
   </div>
 );

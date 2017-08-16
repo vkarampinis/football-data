3238f3383c0ce9dfaafd44c3179a630a84665bd4
Vaios Karampinis <oden@oden.gr>
2017-08-16 22:16:07 +0300

    update menu

--- src/components/SeasonMenu.js
+++ src/components/SeasonMenu.js
@@ -7,17 +7,17 @@ const SeasonMenu = () => (
     <Menu.Item>Season Year:</Menu.Item>
     <Route path="/competitions/2017" children={({match}) => (
       <Link to="/competitions/2017">
-        <Menu.Item name="2017" active={!!match}/>
+        <Menu.Item name="2017/2018" active={!!match}>2017-2018</Menu.Item>
       </Link>
     )}/>
     <Route path="/competitions/2016" children={({match}) => (
       <Link to="/competitions/2016">
-        <Menu.Item name="2016" active={!!match}/>
+        <Menu.Item name="2016/2017" active={!!match}>2016-2017</Menu.Item>
       </Link>
     )}/>
     <Route path="/competitions/2015" children={({match}) => (
       <Link to="/competitions/2015">
-        <Menu.Item name="2015" active={!!match}/>
+        <Menu.Item name="2015/2016" active={!!match}>2015-2016</Menu.Item>
       </Link>
     )}/>
   </Menu>

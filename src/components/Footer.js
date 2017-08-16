eca87bf55009e9894d011f5c60cb35d54397e84f
Vaios Karampinis <oden@oden.gr>
2017-08-16 23:11:35 +0300

    fix broken link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const SeasonMenu = () => (
     <Divider/>
     <Header as='h5' color='grey' textAlign='center'>
       This page was created with <span role="img">❤</span> and some frozen <span role="img">🍺</span>.<br/>
-      Fill free to <a href="https://github.com/vkarampinis/football-stats" target="_blank" rel="noopener noreferrer">fork</a>, hack and learn! <br/>
+      Fill free to <a href="https://github.com/vkarampinis/football-data" target="_blank" rel="noopener noreferrer">fork</a>, hack and learn! <br/>
       All data provided by open API from <a href="http://football-data.org" target="_blank" rel="noopener noreferrer">football-data.org</a>
     </Header>
   </div>

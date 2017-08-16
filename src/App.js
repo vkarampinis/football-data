7838ff3fcd9d6b4452cd58493dfe57f487441e7e
Vaios Karampinis <oden@oden.gr>
2017-08-16 22:22:34 +0300

    remove unused import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Container, Grid, Header} from 'semantic-ui-react'
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import SeasonMenu from './components/SeasonMenu'
 import BrowseCompetition from './components/BrowseCompetition'
 import BrowseTeams from './components/BrowseTeams'
